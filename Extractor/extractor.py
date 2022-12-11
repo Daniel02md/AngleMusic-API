0f7841f130807a76db7de0bd4603a47a3efe7d45
Daniel02md <gustanunespp157@gmail.com>
2022-12-11 07:53:20 -0300

    Set a custom User-Agent to yt-dl

--- Extractor/extractor.py
+++ Extractor/extractor.py
@@ -1,6 +1,6 @@
 from json import dumps
 from youtube_dl.YoutubeDL import YoutubeDL
-
+import youtube_dl.utils
 
 class Extractor:
     def __init__(self) -> None:
@@ -12,7 +12,9 @@ class Extractor:
         yt_opts = {
             'format': '140/bestaudio',
             'quiet': True
-        }
+        }   
+        youtube_dl.utils.std_headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
+
         yt_dl = YoutubeDL(yt_opts)
         arrayformats = yt_dl.extract_info(uriVideo.replace('"', ''), download=False)['formats']
         for item in arrayformats:
