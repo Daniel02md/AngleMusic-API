6e81068e50594af9fa19d21fbdcb220da3042d01
Daniel B <daniel.marcos.bezerra@gmail.com>
2023-05-06 19:49:25 -0300

    Update auth.py - Inactivating credentials.

--- Authentication/auth.py
+++ Authentication/auth.py
@@ -3,8 +3,8 @@ from googleapiclient.discovery import build
 class Auth:
     
     def __init__(self, keyAuth = 'AIzaSyCTY4ExbPCJrOH-1tOSAZr_6cbiyJzU6iw'):
-        #key 1: AIzaSyCTY4ExbPCJrOH-1tOSAZr_6cbiyJzU6iw
-        #key 2: AIzaSyA33m-lnUKd5bHdVm7Kvjw2C-GenJMpja4
+        #key 1: AIzaSyCTY4ExbPCJrOH-1tOSAZr_6cbiyJzU6iw (inactive)
+        #key 2: AIzaSyA33m-lnUKd5bHdVm7Kvjw2C-GenJMpja4 (inactive)
         self.__SERVICE_NAME = 'youtube'
         self.__SERVICE_VERSION = 'v3'
         self.keyAuth = keyAuth
